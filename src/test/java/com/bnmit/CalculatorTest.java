198b1f43d45cabf6ea675a6b10a56d9f4e7f7ea5
Rachana1472 <sbrachana571@gmail.com>
2026-04-23 23:41:08 +0530

    Add files via upload

---
+++ src/test/java/com/bnmit/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.bnmit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(10, calc.add(5, 5));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2, calc.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(15, calc.multiply(3, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calc.divide(10, 5));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+    }
+}
